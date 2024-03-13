70f1f07fe70153db23855d62ee3705e98aa82655
harsha01raj <harshraj810k@gmail.com>
2024-03-13 22:02:53 +0530

    backend

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken');
 const {blackList}=require('../blackList.js');
 
 const auth=(req,res,next)=>{
-    const token=req.headers.authorization.split(' ')[1];
+    const token=req.headers.authorization;
     if(token){
         if(blackList.includes(token)){
             res.send({'Message':"Please login again"});
